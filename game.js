68ec745824d2faf1e6e819f5aab8c608252cd093
Isaiah Raya <rayaisaiah@gmail.com>
2016-11-15 11:20:50 -0800

    added a barrirer so you cant go off screen

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 //movement
 var metax = Number(document.getElementById("meta").getAttribute("x"));
 document.addEventListener("keydown", function(e) {
- if (e.keyCode == 37) {
+ if (e.keyCode == 37 && metax + 10 > 0) {
     document.getElementById("meta").setAttribute("x",metax-10)
   metax = metax-10
  }
- else if (e.keyCode == 39){
+ else if (e.keyCode == 39 && metax + 10 <200 ){
    document.getElementById("meta").setAttribute("x",metax+10)
   metax = metax+10
  }
@@ -13,11 +13,11 @@ document.addEventListener("keydown", function(e) {
 })
 var metay = Number(document.getElementById("meta").getAttribute("y"));
 document.addEventListener("keydown", function(e) {
- if (e.keyCode == 38) {
+ if (e.keyCode == 38 && metay + 10 > 0) {
     document.getElementById("meta").setAttribute("y",metay-10)
   metay = metay-10
  }
- else if (e.keyCode == 40){
+ else if (e.keyCode == 40 && metay + 10 < 95){
    document.getElementById("meta").setAttribute("y",metay+10)
   metay = metay+10
  }
